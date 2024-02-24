786c66c3fb248b59e7c6b66608b5795a20a1f61c
Adedoyin Daniel <134150745+Nielads123@users.noreply.github.com>
2024-02-24 19:52:40 +0100

    Add files via upload

---
+++ STEM/stem.cpp
@@ -0,0 +1,57 @@
+// program to calculate surface area of a triangle, rectangle and square
+
+#include <iostream>
+using namespace std;
+int main(){
+	int enter;
+	cout<<"program to calculate surface area of a triangle, rectangle and square"<<endl;
+	cout<<"press 1 to calculate the surface area of a triangle"<<endl;
+	cout<<"press 2 to calculate the surface area of a square"<<endl;
+	cout<<"press 3 to calculate the surface area of a rectangle"<<endl;
+	cout<<"Enter the surface area you want to calculate first: "<<endl;
+	cin>>enter;
+	if(enter == 1){
+		cout<<"You are calculating the surface area of a triangle"<<endl;
+		int base,height;
+		int sum;
+		cout<<"The formular for the surface area of a triangle is: 1/2bh"<<endl;
+		cout<<"Enter a number for the base: "<<endl;
+		cin>>base;
+		
+		cout<<"Enter a number for the height: "<<endl;
+		cin>>height;
+		sum = 0.5*base*height;
+		cout<<"your answer is: "<<sum<<endl;
+	}
+	
+	else if(enter ==2){
+		cout<<"You are calculating the surface area of a square"<<endl;
+			cout<<"The formular for the surface area of a square is: S^2"<<endl;
+			int distance,sum;
+			cout<<"Enter a number for the distance: "<<endl;
+			cin>>distance;
+			
+			sum = distance* distance;
+			cout<<"your answer is: "<<sum<<endl;
+	}
+
+	else if(enter == 3){
+		cout<<"You are calculating the surface area of a rectangle"<<endl;
+		cout<<"The formular for the surface area of a rectangle is: length * breadth(l*b)"<<endl;
+		int length, breadth,sum;
+		cout<<"Enter a number for the length: "<<endl;
+		cin>>length;
+		
+		cout<<"Enter a value for the breadth: ";
+		cin>>breadth;
+		
+		sum= length * breadth;
+		cout<<"your answer is: "<<sum<<endl;
+	}
+	
+	else{
+		cout<<"Pick a number"<<endl;
+	}
+
+return 0;
+}
---
+++ STEM/stem2.cpp
@@ -0,0 +1,26 @@
+// program to calculate object in 2D dimensional space
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	cout<<"program to calculate object in 2D dimensional space \n";
+	cout<< "The formmular is sqrt(x2 - x1)^2 + (y2 - y1)^2"<<endl;
+	
+	int x2, x1, y2 ,y1,sum;
+
+
+	cout<<"Enter a value for x2"<<endl;
+	cin>>x2;
+	
+	cout<<"Enter a value for x1"<<endl;
+	cin>>x1;
+	
+	cout<<"Enter a value for y2"<<endl;
+	cin>>y2;
+	
+	cout<<"Enter a value for y1"<<endl;
+	cin>>y1;
+	
+	sum =  sqrt(x2 - x1)*(x2 -x1)+ (y2 - y1)*(y2 - y1);
+	cout<<"Your answer is: "<<sum<<endl;
+}
