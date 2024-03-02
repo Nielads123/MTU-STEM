14f6f14a4ce4bc6503edb2abd9478b74013d176d
Adedoyin Daniel <134150745+Nielads123@users.noreply.github.com>
2024-03-02 21:12:17 +0100

    Update stem2.cpp

--- STEM/stem2.cpp
+++ STEM/stem2.cpp
@@ -6,7 +6,7 @@ int main(){
 	cout<<"program to calculate object in 2D dimensional space \n";
 	cout<< "The formmular is sqrt(x2 - x1)^2 + (y2 - y1)^2"<<endl;
 	
-	int x2, x1, y2 ,y1,sum;
+	float x2, x1, y2 ,y1,sum;
 
 
 	cout<<"Enter a value for x2"<<endl;
@@ -23,4 +23,33 @@ int main(){
 	
 	sum =  sqrt(x2 - x1)*(x2 -x1)+ (y2 - y1)*(y2 - y1);
 	cout<<"Your answer is: "<<sum<<endl;
+
+
+	cout<<"Midpointt of both axis"<<endl;
+	int X2, X1, Y2 ,Y1;
+	cout<<"Enter a value for x2"<<endl;
+	cin>>X2;
+	
+	cout<<"Enter a value for x1"<<endl;
+	cin>>X1;
+	
+	
+	float midpointx;
+	midpointx = (X2+X1)/2;
+	cout<<"Your answer is: "<<midpointx<<endl;
+	
+	cout<<"Enter a value for y2"<<endl;
+	cin>>Y2;
+	
+	cout<<"Enter a value for y1"<<endl;
+	cin>>Y1;
+	
+	cout<<"Midpoint of x axis"<<endl;
+	
+	
+		float midpointy;
+	midpointy = (Y2+Y1/)2;
+	cout<<"Your answer is: "<<midpointy<<endl;
+	
+
 }
